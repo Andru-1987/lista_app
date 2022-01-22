19b86489b6b74b4d140dd2fcde2fb56e044163fd
Anderson <andru.ocatorres@gmail.com>
2022-01-22 08:46:02 -0300

    new changes

---
+++ src/CustomComponent/Clock.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import useClock from "../Hooks/useClock";
+import styled from "styled-components"
+
+const ClockFrame=styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@100&family=Noto+Sans+JP:wght@100&family=Rampart+One&display=swap');
+    display:flex;
+    justify-content:center;
+    width:100%;
+
+    font-family: 'Josefin Slab', serif;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-family: 'Rampart One', cursive;
+    cursor:pointer;
+`
+
+
+const Clock=()=>{
+
+    const [time,show,displayClock]=useClock();
+
+    return (
+        <>
+            
+                <ClockFrame onClick={displayClock}>
+                    {show? <p>
+                        {time}
+                        </p>
+                         : "Click me: I'm hidden Clock ≧☉_☉≦"}
+                 </ClockFrame>
+            
+
+        </>
+    )
+ }
+
+ export default Clock;
---
+++ src/Hooks/useClock.js
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from "react";
+
+const useClock=()=>{
+
+    const [time,setTime]=useState(new Date().toLocaleTimeString());
+    const [show,setShow]=useState(true);
+
+    const timeSetter=()=>{
+        setTime(new Date().toLocaleTimeString());
+    }
+
+    useEffect(()=>{
+        setInterval(()=>timeSetter(),1000);
+        return ()=>{
+            clearInterval(timeSetter());
+        }
+    })
+
+    const displayClock=()=>{
+        console.log('show  clock');
+        setShow(prevState=>!prevState);
+
+    };
+  
+
+    return  [time,show,displayClock]
+}
+export default useClock;
